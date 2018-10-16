73a62ef95ba6ed2558d48b377e49438a35e241fe
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-10-16 15:04:38 -0700

    add initial startin files and directories

---
+++ src/app/mock-recipes.ts
@@ -0,0 +1,58 @@
+import { Recipe } from './models/recipe.models';
+
+export const RECIPES: Recipe[] = [
+  {
+    title: 'Pumpkin Pie',
+    ingredients: [
+      '2 eggs plus the yolk of a third egg',
+      '1/2 cup packed dark brown sugar',
+      '1/3 cup white sugar',
+      '1/2 teaspoon salt',
+      '2 teaspoons of cinnamon',
+      '1 teaspoon ground ginger',
+      '1/4 teaspoon ground nutmeg',
+      '1/4 teaspoon ground cloves',
+      '1/8 teaspoon ground cardamom',
+      '1/2 teaspoon of lemon zest',
+      '2 cups of pumpkin pulp purée from a sugar pumpkin* OR 1 15-ounce can of pumpkin purée (can also use puréed cooked butternut squash)',
+      '1 1/2 cup heavy cream or 1 12 oz. can of evaporated milk',
+      '1 good crust (see pâte brisée recipe or our no-fail flaky pie crust recipe)'
+    ],
+    directions: [
+      'Preheat your oven to 425°F.',
+      'Beat eggs, mix in sugars, salt, spices, lemon zest: Beat the eggs in a large bowl. Mix in the brown sugar, white sugar, salt, spices—cinnamon, ground ginger, nutmeg, ground cloves, cardamom, and lemon zest.',
+      'Add pumpkin and cream: Mix in the pumpkin purée. Stir in the cream. Beat together until everything is well mixed.',
+      'Pour into pie shell and bake: Pour the filling into an uncooked pie shell. Bake at a high temperature of 425°F for 15 minutes.',
+      'Then after 15 minutes, lower the temperature to 350°F. Bake for 45 to 55 minutes more. The pie is done when the center of the filling reaches 175°F (79°C) when measured with an instant read thermometer.',
+      'If you don\'t have a thermometer, a knife tip inserted in the center comes should come out wet but relatively clean, and the center of the pie should still be a little bit jiggly.',
+      '(About half-way through the baking, you may want to put foil around the edges or use a pie protector to keep the crust from getting too browned.)',
+      'Cool on a rack: Cool the pumpkin pie on a wire rack for 2 hours. Note that the pumpkin pie will come out of the oven all puffed up (from the leavening of the eggs), and will deflate as it cools.',
+      'Serve with whipped cream.'
+    ]
+  },
+  {
+    title: 'Easy Chocolate Mousse Pie',
+    ingredients: [
+      '2 Tbsp salted butter, diced into 4 pieces',
+      '1 cup semi-sweet chocolate chips*',
+      '1/2 cup bittersweet chocolate chips (or another 1/2 cup semi-sweet if you don\'t want it quite as rich)',
+      '2 cups mini marshmallows',
+      '1/3 cup milk (whole or 2%)',
+      '1 tsp vanilla extract',
+      '1 3/4 cups heavy whipping cream',
+      '1 (6 oz) store-bought Oreo pie crust'
+    ],
+    directions: [
+      'Add butter, milk, semi-sweet chocolate chips, bittersweet chocolate chips and marshmallows to a large saucepan.',
+      'Heat over low heat, stirring constantly until chocolate and marshmallows have melted (it will take a while for the marshmallows to melt, keep stirring).',
+      'Remove from heat, stir in vanilla pour into a large mixing bowl and let cool, about 45 - 60 minutes (it should be about room temperature).',
+      'Whip 1 3/4 cups heavy cream until very stiff peaks form. Fold into cool chocolate mixture.',
+      'Pour chocolate mixture into Oreo crust and spread into an even layer (if you don\'t want it as tall as mine is you can just reserve some of the mousse chilled in cups for later).',
+      'Chill in refrigerator 3 hours.',
+      'For the topping, whip remaining 1 cup cream until soft peaks form. Add in sugar and whip until very stiff peaks form.',
+      'Spread over pie, garnish with chocolate then cut into slices. Store pie in refrigerator.',
+      '*I\'ve also made this successfully with 1 cup milk chocolate chips and 1/2 cup semi-sweet chocolate chips for more of a milk chocolate flavor vs. the regular dark chocolate flavor it has.',
+      '**To make chocolate shavings just run a vegetable peeler down the sides of a bar of chocolate.'
+    ]
+  }
+]
